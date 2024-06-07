3d3e5a66e90802411c063a88e139f6e85d141298
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2024-06-07 16:26:09 -0400

    Home page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home= () => {
@@ -15,6 +16,12 @@ const Home= () => {
           user={loggedIn?.firstName || 'Guest'}
           subtext="Access and manage your account and transactions efficiently"
           />
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+          />
+
         </header>
       </div>
     </section>
