93994768235a1c3cfeedd29b8dcd1c3f0de8a72c
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2024-06-11 14:09:35 -0400

    Right Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/ui/HeaderBox'
+import RightSidebar from '@/components/ui/RightSidebar';
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
@@ -23,7 +24,14 @@ const Home= () => {
           />
 
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[]}
+      />
     </section>
   )
 }
