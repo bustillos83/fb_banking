d3642ce2b83b4579a3a11cb5b56a77c8c47ffb70
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2024-06-20 15:04:04 -0400

    added home page image

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,6 +5,7 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import Image from "next/image";
+import { redirect, useRouter } from "next/navigation";
 
 
 export default async function RootLayout({
@@ -13,7 +14,11 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
 
+ 
   const loggedIn = await getLoggedInUser();
+
+  if(!loggedIn) redirect('/sign-in')
+
   return (
    <main className="flex h-screen w-full font-inter">
     <Sidebar user={loggedIn} />
