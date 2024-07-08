29b1ccbf46d3609e34160c4b9bb999723b58a7f4
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2024-07-08 16:15:32 -0400

    working on adding banks

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { ID } from "node-appwrite"
+import { ID, Query } from "node-appwrite"
 import { createAdminClient, createSessionClient } from "../appwrite"
 import { cookies } from "next/headers"
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils"
@@ -231,4 +231,19 @@ export const exchangePublicToken = async ({
     
  } 
 
+ }
+
+ export const getBanks = async ({ userId }: getBanksProps) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const banks = await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('userId', [userId])]
+    )
+    return parseStringify(banks.documents);
+  } catch (error) {
+    console.log(error)
+  }
  }
